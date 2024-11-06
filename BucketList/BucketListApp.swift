e0fe75a9dc4cc0ad9f741dddabfdbc93912667b4
sxmtee <somtoo76@gmail.com>
2024-11-06 16:55:47 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by mac on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
