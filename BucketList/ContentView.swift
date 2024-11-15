a3bb3a32aedabb0bac61cf1c4be2b27512246adc
sxmtee <somtoo76@gmail.com>
2024-11-15 16:24:56 +0100

    BucketList Project Done

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -16,48 +16,65 @@ struct ContentView: View {
         )
     )
     
-    @State private var locations = [Location]()
-    @State private var selectedPlace: Location?
+    @State private var viewModel = ViewModel()
+    @AppStorage("mapStyle") private var mapStyle = "standard"
     
     var body: some View {
-        MapReader { proxy in
-            Map(initialPosition: startPosition) {
-                ForEach(locations) { location in
-                    Annotation(
-                        location.name,
-                        coordinate: location.coordinate
-                    ) {
-                        Image(systemName: "star.circle")
-                            .resizable()
-                            .foregroundStyle(.red)
-                            .frame(width: 44, height: 44)
-                            .background(.white)
-                            .clipShape(.circle)
-                            .onLongPressGesture {
-                                selectedPlace = location
+        if viewModel.isUnlocked {
+            VStack {
+                MapReader { proxy in
+                    Map(initialPosition: startPosition) {
+                        ForEach(viewModel.locations) { location in
+                            Annotation(
+                                location.name,
+                                coordinate: location.coordinate
+                            ) {
+                                Image(systemName: "star.circle")
+                                    .resizable()
+                                    .foregroundStyle(.red)
+                                    .frame(width: 44, height: 44)
+                                    .background(.white)
+                                    .clipShape(.circle)
+                                    .onLongPressGesture {
+                                        viewModel.selectedPlace = location
+                                    }
                             }
+                        }
+                    }
+                    .mapStyle(mapStyle == "standard" ? .standard : .hybrid)
+                    .onTapGesture { position in
+                        if let coordinate = proxy.convert(position, from: .local) {
+                            viewModel.addLocation(at: coordinate)
+                        }
+                    }
+                    .sheet(item: $viewModel.selectedPlace) { place in
+                        EditView(location: place) {
+                            viewModel.update(location: $0)
+                        }
                     }
                 }
-            }
-            .onTapGesture { position in
-                if let coordinate = proxy.convert(position, from: .local) {
-                    let newLocation = Location(
-                        id: UUID(),
-                        name: "New location",
-                        description: "",
-                        latitude: coordinate.latitude,
-                        longitude: coordinate.longitude
-                    )
-                    locations.append(newLocation)
+                
+                Picker("Map mode", selection: $mapStyle) {
+                    Text("Standard")
+                        .tag("standard")
+
+                    Text("Hybrid")
+                        .tag("hybrid")
                 }
+                .pickerStyle(.segmented)
+                .padding(.horizontal)
             }
-            .sheet(item: $selectedPlace) { place in
-                EditView(location: place) { newLocation in
-                    if let index = locations.firstIndex(of: place) {
-                        locations[index] = newLocation
-                    }
+        } else {
+            Button("Unlock Places", action: viewModel.authenticate)
+                .padding()
+                .background(.blue)
+                .foregroundStyle(.white)
+                .clipShape(.capsule)
+                .alert("Authentication error", isPresented: $viewModel.isShowingAuthenticationError) {
+                    Button("OK") { }
+                } message: {
+                    Text(viewModel.authenticationError)
                 }
-            }
         }
     }
 }
